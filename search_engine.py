8507e663ae37967e2498e03e452a2d1e2094a706
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-10 01:07:16 -0400

    Added functions to Python file and csv file with data

--- search_engine.py
+++ search_engine.py
@@ -15,6 +15,7 @@ with open('players.csv', 'r') as f:
 
 
 def sport_engine():
+    print("\nWelcome to the Sports Search Engine of players in the English Premier League!\n")
     while True:
         print("To insert new player type [i]")
         print("To update player's info type [u]")
@@ -29,16 +30,40 @@ def sport_engine():
             input('Enter age: '), input('Enter country: '), input('Enter if player is a captain: ')))
 
         elif choice == "d":
-            cur.execute("DELETE FROM players WHERE name = %s",
-            (input('choose name to delete: '),))
+            cur.execute("DELETE FROM players WHERE name = %s", (input('Choose player to delete: '),))
 
         elif choice == "u":
-            cur.execute("UPDATE players SET height=(%s) WHERE name = (%s)", (input('Enter new height: '), input('Enter name: ')))
+            print("To update position type [p]")
+            print("To update jersey number type [j]")
+            print("To update team type [t]")
+            print("To update age type [a]")
+            #print("To exit type [x]")
+            pick = input("Choose from the above: ")
+            if pick == "p":
+                cur.execute("UPDATE players SET position = %s WHERE name = %s", (input('Update position: '), input('Enter name: ')))
+                #results = cur.fetchone()
+                # conn.commit()
+                #print(*results, sep = "\n")
+            elif pick == "j":
+                cur.execute("UPDATE players SET jersey_number = %s WHERE name = %s", (input('Update player\'s number: '), input('Enter name: ')))
+                #results = cur.fetchall()
+                # conn.commit()
+                #print(*results, sep = "\n")
+            elif pick == "t":
+                cur.execute("UPDATE players SET club = %s WHERE name = %s", (input('Update club: '), input('Enter name: ')))
+                #results = cur.fetchall()
+                # conn.commit()
+                #print(*results, sep = "\n")
+            elif pick == "a":
+                cur.execute("UPDATE players SET age = %s WHERE name = %s", (input('Update player\'s age: '), input('Enter name: ')))
+                #results = cur.fetchall()
+                # conn.commit()
+                #print(*results, sep = "\n")
 
         elif choice == "s":
             print("To search by name type [n]")
-            print("To search by position [p]")
-            print("To search by jersey number [j]")
+            print("To search by position type [p]")
+            print("To search by jersey number type [j]")
             print("To search if player is a caprain type [c]")
             print("To search for a team type [t]")
             #print("To exit type [x]")
@@ -71,15 +96,16 @@ def sport_engine():
             # print(type(results[1]))
 
         elif choice == "x":
+            print("Have a great day!")
             break
         # else:
         #     pass
-        conn.commit()
+        #conn.commit()
 
 sport_engine()
 #
 # cur.execute("SELECT * FROM players;")
-#cur.fetchall()
+# cur.fetchall()
 conn.commit()
 
 
